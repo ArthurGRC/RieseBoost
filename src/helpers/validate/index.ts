a368d2abaea70ddef4ec29ecca09772613a52428
Arthur <arthurriesecorrea@gmail.com>
2024-08-27 22:23:16 -0300

    chore: add validations in user model

---
+++ src/helpers/validate/index.ts
@@ -0,0 +1,9 @@
+export const userValidate = {
+  notNullRole: 'Role não pode ficar em branco',
+  notNullPassword: 'Senha não pode ficar em branco',
+  notNullEmail: 'Email não pode ficar em branco',
+  notNullName: 'Nome nao pode ficar em branco',
+  wrongEmail: 'Email inválido',
+  wrongPassword: 'Senha inválida, deve conter ao menos 9 caracteres, tendo números, caractere especial e letras maiúsculas',
+  uniqueEmail: 'Email já está sendo utilizado'
+}
