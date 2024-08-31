a9fc493a4c40f95cfa19ba3652042e157e94245a
Arthur <arthurriesecorrea@gmail.com>
2024-08-31 00:00:43 -0300

    chore: adjust api login

---
+++ src/helpers/constants/errors.ts
@@ -0,0 +1,14 @@
+export const UNAUTHORIZED_ERROR = {
+  message: 'Usuário não autorizado',
+  path: 'unauthorized'
+}
+
+export const INCORRECT_PASSWORD = {
+  message: 'Senha incorreta, tente novamente',
+  path: 'password'
+}
+
+export const NOT_FOUND_USER = {
+  message: 'Usuário não encontrado, tente novamente',
+  path: 'email'
+}
