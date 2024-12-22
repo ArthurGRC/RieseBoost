fa66714c33782fc43af1f6c5470f46206c830ab2
Arthur Gabriel <arthur@MacBook-Air-de-Arthur.local>
2024-12-22 20:44:02 -0300

    chore(config-migrate): add pg and migration for create user table

---
+++ migrations/20241222233744-create-user.js
@@ -0,0 +1,39 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        isNumeric: true,
+        min: 11,
+        max: 11,
+      },
+      roles: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  },
+};
